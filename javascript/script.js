6ca8aa2a294ee70b61bf5d678b4ae6b3074b58f0
Pankaj Kumar <pankaj14061998@gmail.com>
2018-04-11 08:13:06 +0530

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,40 @@
+function sum()
+		{
+			var a=Number(document.getElementById("1").value);
+			var b=Number(document.getElementById("2").value);
+		
+			var c=a+b;
+			document.getElementById("answer").value=c;
+		}
+function subtraction()
+		{
+			var a=Number(document.getElementById("1").value);
+			var b=Number(document.getElementById("2").value);
+		
+			var c=a-b;
+			document.getElementById("answer").value=c;
+		}
+function multiply()
+		{
+			var a=Number(document.getElementById("1").value);
+			var b=Number(document.getElementById("2").value);
+		
+			var c=a*b;
+			document.getElementById("answer").value=c;
+		}
+function divison()
+		{
+			var a=Number(document.getElementById("1").value);
+			var b=Number(document.getElementById("2").value);
+		
+			var c=a/b;
+			document.getElementById("answer").value=c;
+		}
+function percentage()
+		{
+			var a=Number(document.getElementById("1").value);
+			var b=Number(document.getElementById("2").value);
+		
+			var c=(a/b)*100;
+			document.getElementById("answer").value=c;
+		}
